93bc37c9e7e75e40f09d2fce9aef92634f834338
Jean-Francois Duval <jfduval@aqra.ca>
2018-02-22 16:10:36 -0500

    Added PROJECT_POCKET_2XDC

--- inc/user-mn.h
+++ inc/user-mn.h
@@ -63,6 +63,7 @@ void user_fsm_2(void);
 #define PROJECT_ANKLE_2DOF		4	//Biomechatronics 2-DOF Ankle
 #define PROJECT_RICNU_KNEE		5	//RIC/NU Knee
 #define PROJECT_UMICH_KNEE		6	//University of Michigan's Knee
+#define PROJECT_POCKET_2XDC		7
 
 //List of sub-projects:
 #define SUBPROJECT_NONE			0
@@ -78,8 +79,8 @@ void user_fsm_2(void);
 //Step 1) Select active project (from list):
 //==========================================
 
-#define ACTIVE_PROJECT			PROJECT_ACTPACK
-#define ACTIVE_SUBPROJECT		SUBPROJECT_B
+#define ACTIVE_PROJECT			PROJECT_POCKET_2XDC
+#define ACTIVE_SUBPROJECT		SUBPROJECT_A
 
 //Step 2) Customize the enabled/disabled sub-modules:
 //===================================================
@@ -341,6 +342,30 @@ void user_fsm_2(void);
 	#define RUNTIME_FSM2		ENABLED
 
 #endif	//PROJECT_BB_RIGID
+
+//FlexSEA-Pocket
+#if(ACTIVE_PROJECT == PROJECT_POCKET_2XDC)
+
+
+	//Enable/Disable sub-modules:
+	#define USE_USB
+	#define USE_COMM			//Requires USE_RS485 and/or USE_USB
+	#define USE_I2C_1			//3V3, IMU & Digital pot
+	//#define USE_I2C_2			//3V3, Expansion
+	#define USE_I2C_3			//Onboard, Regulate & Execute
+	#define USE_IMU				//Requires USE_I2C_1
+	#define USE_UART3			//Bluetooth
+	#define USE_EEPROM			//Emulated EEPROM, onboard FLASH
+	#define USE_WATCHDOG		//Independent watchdog (IWDG)
+	//#define USE_SPI_PLAN		//Enables the external SPI port
+
+	//Runtime finite state machine (FSM):
+	//#define RUNTIME_FSM1		ENABLED	//Enable only if you DO NOT use Plan
+	#define RUNTIME_FSM2		ENABLED	//Enable at all time, Mn <> Ex comm.
+
+	#define MULTI_DOF_N			0
+
+#endif	//PROJECT_ACTPACK
 
 #if(ACTIVE_PROJECT == PROJECT_DEPHY)
 
