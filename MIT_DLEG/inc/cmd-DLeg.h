713217165c7d2bf958095151f2470a2eb76fc438
tshu <tshu6@gatech.edu>
2018-03-07 03:02:11 -0500

    cmd-DLeg files

---
+++ MIT_DLEG/inc/cmd-DLeg.h
@@ -0,0 +1,50 @@
+#ifndef INC_FLEXSEA_CMD_DLEG_H
+#define INC_FLEXSEA_CMD_DLEG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//****************************************************************************
+// Include(s)
+//****************************************************************************
+
+#include <stdint.h>
+#include "flexsea_user_structs.h"
+#include "state_machine.h"
+
+//****************************************************************************
+// RX/TX Prototype(s):
+//****************************************************************************
+
+void rx_cmd_dleg_rw(uint8_t *buf, uint8_t *info);
+void rx_cmd_dleg_rr(uint8_t *buf, uint8_t *info);
+
+void tx_cmd_dleg_r(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, \
+					uint16_t *len, uint8_t setNumber);
+void tx_cmd_dleg_rw(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, \
+					uint16_t *len, uint8_t setNumber);
+void tx_cmd_dleg_w(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, \
+					uint16_t *len, uint8_t setNumber);
+
+//****************************************************************************
+// Prototype(s) - simplified functions (DLL):
+//****************************************************************************
+
+//****************************************************************************
+// Definition(s):
+//****************************************************************************
+
+//****************************************************************************
+// Structure(s):
+//****************************************************************************
+
+//****************************************************************************
+// Shared variable(s)
+//****************************************************************************
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	//INC_FLEXSEA_CMD_DLEG_H
