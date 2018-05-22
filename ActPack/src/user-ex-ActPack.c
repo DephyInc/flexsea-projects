54b1d264d2dbf23817ff9eae4341fe2ff5f8ddd7
Jean-Francois Duval <jfduval@dephy.com>
2018-05-22 09:41:23 -0400

    Moved common code from ActPack to user-ex-rigid.c

--- ActPack/src/user-ex-ActPack.c
+++ ActPack/src/user-ex-ActPack.c
@@ -28,67 +28,39 @@
 #include "user-ex-rigid.h"
 #include "flexsea_user_structs.h"
 #include "flexsea_sys_def.h"
-#include "filters.h"
 #include "flexsea_board.h"
-#include "control.h"
-#include "mem_angle.h"
-#include "motor.h"
 
 //****************************************************************************
 // Variable(s)
 //****************************************************************************
 
-struct diffarr_s mot_ang_clks;
-
-int32_t mot_ang = 0;
-int32_t mot_ang_offset = 0; //initialized offset to match with calibration
-int32_t mot_vel = 0;
-int32_t mot_acc = 0;
-
-int16_t ank_to_mot[320];
-
 //****************************************************************************
 // Private Function Prototype(s):
 //****************************************************************************  
-
 
 //****************************************************************************
 // Public Function(s)
 //****************************************************************************
 
-//Call this function once in main.c, just before the while()
+//Common setup done in user-ex-rigid/init_project()
+//Add extra steps here
 void initActPack(void)
 {
-	setBoardID(SLAVE_ID);
-	
-	//Controller setup:
-	ctrl[0].active_ctrl = CTRL_NONE;		//No controller at boot
-	setMotorVoltage(0,0);				//0% PWM
+	//...
 }
 
-//Knee Finite State Machine.
+//Finite State Machine.
 //Call this function in one of the main while time slots.
 void ActPack_fsm(void)
 {
 	ActPack_refresh_values();
 }
 
-//Here's an example function:
+//Common variables are refreshed in user-ex-rigid/refresh_values()
+//Add extra variables here
 void ActPack_refresh_values(void)
 {	
-	mot_ang = *rigid1.ex.enc_ang - mot_ang_offset;
-	mot_vel = *exec1.enc_ang_vel; //cpms
-	
-	update_diffarr(&mot_ang_clks, mot_ang, 10);
-	
-	mot_acc = get_accl_1k_5samples_downsampled(&mot_ang_clks)/2609; //rad/s^2
-	rigid1.ex.mot_acc = mot_acc;
-	
-	#ifdef BOARD_SUBTYPE_POCKET
-	//ToDo second channel
-	pocket1.ex[0].mot_acc = rigid1.ex.mot_acc;
-	pocket1.ex[1].mot_acc = rigid1.ex.mot_acc;
-	#endif
+	//...
 }
 
 //****************************************************************************
--- src/user-ex-rigid.c
+++ src/user-ex-rigid.c
@@ -22,6 +22,11 @@
 //****************************************************************************
 
 #include "../inc/user-ex-rigid.h"
+#include "flexsea_user_structs.h"
+#include "flexsea_sys_def.h"
+#include "filters.h"
+#include "control.h"
+#include "motor.h"
 
 #if((ACTIVE_PROJECT == PROJECT_ACTPACK) || defined CO_ENABLE_ACTPACK)
 #include "user-ex-ActPack.h"
@@ -35,11 +40,18 @@
 // Variable(s)
 //****************************************************************************
 
+struct diffarr_s mot_ang_clks;
+int32_t mot_ang = 0;
+int32_t mot_ang_offset = 0; //initialized offset to match with calibration
+int32_t mot_vel = 0;
+int32_t mot_acc = 0;
+
 //****************************************************************************
 // Private Function Prototype(s):
 //****************************************************************************
 
-static void init_barebone(void);
+static void refresh_values(void);
+static void init_project(void);
 
 //****************************************************************************
 // Public Function(s)
@@ -48,10 +60,8 @@ static void init_barebone(void);
 //Initialization function - call once in main.c, before while()
 void init_user(void)
 {
-	//Barebone:
-	#if(ACTIVE_PROJECT == PROJECT_BAREBONE)
-	init_barebone();
-	#endif	//PROJECT_BAREBONE
+	//Common setup:
+	init_project();
 	
 	//ActPack:
 	#if(ACTIVE_PROJECT == PROJECT_ACTPACK)
@@ -66,6 +76,9 @@ void init_user(void)
 //Call this function in one of the main while time slots.
 void user_fsm(void)
 {
+	//Refresh sensor values - common across projects:
+	refresh_values();
+	
 	//ActPack:
 	//(Note: Biomech's projects use ActPack)
 	#if((ACTIVE_PROJECT == PROJECT_ACTPACK) || defined CO_ENABLE_ACTPACK)
@@ -85,12 +98,32 @@ void user_fsm(void)
 // Private Function(s)
 //****************************************************************************
 
-static void init_barebone(void)
+//Common setup
+static void init_project(void)
 {
-	//Barebone:
-	#if(ACTIVE_PROJECT == PROJECT_BAREBONE)
 	setBoardID(SLAVE_ID);
-	#endif	//PROJECT_BAREBONE
+	
+	//Controller setup:
+	ctrl[0].active_ctrl = CTRL_NONE;	//No controller at boot
+	setMotorVoltage(0, 0);				//0% PWM
+}
+
+//Refresh sensor values - common across projects
+static void refresh_values(void)
+{	
+	mot_ang = *rigid1.ex.enc_ang - mot_ang_offset;
+	mot_vel = *exec1.enc_ang_vel; //cpms
+	
+	update_diffarr(&mot_ang_clks, mot_ang, 10);
+	
+	mot_acc = get_accl_1k_5samples_downsampled(&mot_ang_clks)/2609; //rad/s^2
+	rigid1.ex.mot_acc = mot_acc;
+	
+	#ifdef BOARD_SUBTYPE_POCKET
+	//ToDo second channel
+	pocket1.ex[0].mot_acc = rigid1.ex.mot_acc;
+	pocket1.ex[1].mot_acc = rigid1.ex.mot_acc;
+	#endif
 }
 
 #endif //BOARD_TYPE_FLEXSEA_EXECUTE
