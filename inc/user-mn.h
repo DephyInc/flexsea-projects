33d4582499dab1bf2d189a7c0fdab54433ccea40
Mike Murray <mmurray@dephy.com>
2019-01-29 12:51:26 -0500

    adding #defines for rigid2.0 manage firmware

--- inc/user-mn.h
+++ inc/user-mn.h
@@ -297,7 +297,7 @@ void reset_user_code(void);
 
 		#endif
 
-	#else
+	#elif (HW_VER < 20) 	        //RIGID 1.0
 
 		//Enable/Disable sub-modules:
 		#define USE_USB
@@ -331,6 +331,51 @@ void reset_user_code(void);
 			#define EXO_SIDE	LEFT
 			#define BILATERAL_SLAVE
 			#define USE_UART3			//Bluetooth #1
+
+		#else
+
+			#error "PROJECT_ACTPACK requires a subproject (use A by default)!"
+
+		#endif
+
+	#else                         	//RIGID 2.0
+
+		//Enable/Disable sub-modules:
+		#define USE_USB
+		#define USE_COMM			//Requires USE_RS485 and/or USE_USB
+		#define USE_I2C_1			//3V3, IMU & Digital pot
+		//#define USE_I2C_2			//3V3, Expansion
+		#define USE_I2C_3			//Onboard, Regulate & Execute
+		#define USE_IMU				//Requires USE_I2C_1
+		//#define USE_UART3			//Bluetooth #1
+		#define USE_EEPROM			//Emulated EEPROM, onboard FLASH
+		//#define USE_WATCHDOG		//Independent watchdog (IWDG)
+		//#define USE_6CH_AMP		//Requires USE_I2C_2. 6-ch Strain Amp.
+		//#define USE_SPI_PLAN		//Enables the external SPI port
+		//#define USE_XB24C			//Radio module on UART2 (Expansion port)
+
+		//Runtime finite state machine (FSM):
+		//#define RUNTIME_FSM1		ENABLED	//Enable only if you DO NOT use Plan
+		#define RUNTIME_FSM2		ENABLED	//Enable at all time, Mn <> Ex comm.
+
+		#define MULTI_DOF_N 		0
+		#define BILATERAL
+
+		#define USE_PARTIAL_PACKETS
+		#define USE_UART3
+		#define USE_BT121
+
+		//#define BT121_UPDATE_MODE
+
+		#if(ACTIVE_SUBPROJECT == RIGHT)
+
+			#define EXO_SIDE	RIGHT
+			#define BILATERAL_MASTER
+
+		#elif(ACTIVE_SUBPROJECT == LEFT)
+
+			#define EXO_SIDE	LEFT
+			#define BILATERAL_SLAVE
 
 		#else
 
