ec9e9b45124f96ae18d84a938417bdc9298ff132
Mike Murray <mmurray393@gmail.com>
2019-06-04 15:20:39 -0400

    added #defines for rigid 3.0 when we're an actpack

--- inc/user-mn.h
+++ inc/user-mn.h
@@ -371,7 +371,7 @@ void reset_user_code(void);
 
 			#endif
 
-		#else 	//RIGID 2.0
+		#elif (HW_VER < 30) 	//RIGID 2.0
 
 			//Enable/Disable sub-modules:
 			#define USE_USB
@@ -398,8 +398,67 @@ void reset_user_code(void);
 			#define BILATERAL
 
 			#define USE_PARTIAL_PACKETS
-			#define USE_UART3
-			#define USE_UART4
+			#define USE_BT121
+
+			// TODO: it looks like when we're configuring the xbee or flashing
+			// new firmware onto the BT121 we need the multi packet stuff
+			// disabled so we just need a good way of setting all the symbols
+
+			#ifdef BT121_UPDATE_MODE
+				#define TEST_BED_ENABLED
+				#define USB_NO_MULTIPACKET
+			#endif
+
+			#ifdef XBEE_CONFIGURATION_MODE
+				#define TEST_BED_ENABLED
+				#define USB_NO_MULTIPACKET
+			#endif
+
+			//#define HABSOLUTE_UPSTREAM_TUNING
+
+			#if(ACTIVE_SUBPROJECT == RIGHT)
+
+				#define EXO_SIDE	RIGHT
+				#define BILATERAL_MASTER
+
+			#elif(ACTIVE_SUBPROJECT == LEFT)
+
+				#define EXO_SIDE	LEFT
+				#define BILATERAL_SLAVE
+
+			#else
+
+				#error "PROJECT_ACTPACK requires a subproject (use A by default)!"
+
+			#endif
+
+		#else				//RIGID 2.0
+
+			//Enable/Disable sub-modules:
+			#define USE_USB
+			#define USE_COMM			//Requires USE_RS485 and/or USE_USB
+			#define USE_I2C_1			//3V3, IMU & Digital pot
+			//#define USE_I2C_2			//3V3, Expansion
+			#define USE_I2C_3			//Onboard, Regulate & Execute
+			#define USE_IMU				//Requires USE_I2C_1
+			#define USE_EEPROM			//Emulated EEPROM, onboard FLASH
+			//#define USE_WATCHDOG		//Independent watchdog (IWDG)
+			//#define USE_6CH_AMP		//Requires USE_I2C_2. 6-ch Strain Amp.
+			//#define USE_SPI_PLAN		//Enables the external SPI port
+			#define USE_XB24C			//Radio module on UART2 (Expansion port)
+			#define USE_PARTIAL_PACKETS
+			#define USE_UART3			//Bluetooth #1
+			#define USE_UART4			//Bluetooth #2
+
+			//Runtime finite state machine (FSM):
+			//#define RUNTIME_FSM1		ENABLED	//Enable only if you DO NOT use Plan
+			#define RUNTIME_FSM2		ENABLED	//Enable at all time, Mn <> Ex comm.
+
+			#define MULTI_DOF_N 		0
+
+			#define BILATERAL
+
+			#define USE_PARTIAL_PACKETS
 			#define USE_BT121
 
 			// TODO: it looks like when we're configuring the xbee or flashing
